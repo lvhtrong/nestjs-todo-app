268d472263b28dbf2e888336287e250dab739b10
Trong Lu <trong.lu@contemi.com.vn>
2021-09-26 18:31:34 +0700

    update app using PORT env instead of 3000

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,6 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.enableCors();
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
